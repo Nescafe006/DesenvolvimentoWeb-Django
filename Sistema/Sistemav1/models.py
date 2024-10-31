84b4445ac804f77b07c60cc5e435a3d74d9ea975
Nescafe006 <153241134+Nescafe006@users.noreply.github.com>
2024-10-31 14:49:37 -0300

    atualization 2

--- Sistema/Sistemav1/models.py
+++ Sistema/Sistemav1/models.py
@@ -7,12 +7,12 @@ class Usuario(models.Model):
     blank=False
     )
     nome_usuario= models.CharField(
-    max_length=254,
+    max_length=255,
     null=False,
     blank=False
     )
     senha = models.CharField(
-    max_length=15,
+    max_length=14,
     null=False,
     blank=False
     )
